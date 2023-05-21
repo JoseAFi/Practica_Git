6f39234adbcd20ca2c4a82fc16cc367d11b65ff0
Jose Figueroa <jofiguer@gmail.com>
2023-05-21 17:32:28 -0300

    "segundo intento de commit"

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,4 +1,5 @@
 """Este sera un archivo de ejemplo
 para practicas de Git y GitHub """
+
 nombre = input('ingrese su nombre: ')
-print ('Buen dia',nombre)
+print ('Buen dia',nombre,'que tengas un excelente día')
