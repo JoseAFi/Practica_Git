6fad1a8bbc771eb6267c07e718462f9f385cce8f
Jose Figueroa <jofiguer@gmail.com>
2023-05-21 21:00:02 -0300

    Cambios a branch main

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,5 +1,5 @@
 """Este sera un archivo de ejemplo
-para practicas de Git y GitHub """
+para practicas de Git y GitHub. Ahora desde rama main"""
 
 nombre = input('ingrese su nombre: ')
 print ('Buen dia',nombre,'que tengas un excelente día')
--- archivo1.py
+++ archivo1.py
@@ -1,1 +1,3 @@
 print('Hola Mundo')
+print('\n')
+print ('Chau Mundo cruel')
